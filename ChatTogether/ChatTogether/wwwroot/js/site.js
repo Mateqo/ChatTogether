25de9f60bc983383020c537e5de7c28728e3ce0f
Bartosz Adamczyk <adamczyk.bartosz@outlook.com>
2021-04-20 16:19:05 +0200

    Dodano poprawny plik css oraz działającą stronę main. Dodano również kos jd, który jest niezbędny do jej działania. Wrzucono również przykładowe zdjęcie profilowe

--- ChatTogether/ChatTogether/wwwroot/js/site.js
+++ ChatTogether/ChatTogether/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const menu = document.querySelector('.nav');
+const btn = document.querySelector('.burger');
+const hamburger = document.querySelector('.fa-bars');
+const iks = document.querySelector('.fa-times');
+const body = document.querySelector('body');
+const main = document.querySelector('.main');
+const navShadow = document.querySelector('.nav-shadow')
+
+
+
+const showing = () => {
+    menu.classList.toggle('active');
+    btn.classList.toggle('active');
+    navShadow.classList.toggle('activeMax');
+    if (menu.classList.contains('active')) {
+        hamburger.classList.add('hide');
+        iks.classList.remove('hide');
+    }
+    else {
+        iks.classList.add('hide');
+        hamburger.classList.remove('hide');
+    };
+    document.body.classList.toggle('lock-scroll');
+    // document.body.classList.toggle('background-shadow');
+};
+// const hideNavOnClickElseWhere = () => {
+
+// }
+
+navShadow.addEventListener('click', showing);
+btn.addEventListener('click', showing);
